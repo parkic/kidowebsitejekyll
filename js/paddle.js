33a28fc795fe9ec4de4500f301ac9d8f082c5bd4
Vladimir Alimpic <vladimir96ks@gmail.com>
2024-08-19 17:10:25 +0200

    add Paddle.js

---
+++ js/paddle.js
@@ -0,0 +1,23 @@
+Paddle.Environment.set("sandbox")
+Paddle.Initialize({
+  token: 'test_96406623aa9a0b2af2a768b393f',
+  pwCustomer: { }
+})
+
+const subscriptionPlans = {
+  monthly: {
+    priceId: 'pri_01j3yykrygkmh960wqmqz3p1nn',
+    quantity: 1
+  },
+  yearly :{  
+    priceId: 'pri_01j3yyqbqs9ncga4awv4v3cf5s',
+    quantity: 1
+  }
+}
+
+function openCheckout(item){
+  console.log(subscriptionPlans[item])
+  Paddle.Checkout.open({
+    items: [subscriptionPlans[item]]
+  });
+}
