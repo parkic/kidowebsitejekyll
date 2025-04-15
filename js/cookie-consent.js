9ffe7b17d3f7ac6791907b8d43fe297b93a39776
Vladimir Alimpic <vladimir96ks@gmail.com>
2025-04-15 18:13:22 +0200

    update website structure for multilang

---
+++ js/cookie-consent.js
@@ -0,0 +1,12 @@
+window.cookieconsent && window.cookieconsent.initialise({
+  palette: {
+    popup: { background: "#000" },
+    button: { background: "#f1d600" }
+  },
+  content: {
+    message: "This website uses cookies to ensure you get the best experience.",
+    dismiss: "Got it!",
+    link: "Learn more",
+    href: "/privacy-policy.html"
+  }
+});
---
+++ js/lang.js
@@ -0,0 +1,22 @@
+function setLanguage(lang) {
+  fetch(`lang/${lang}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelectorAll("[data-i18n]").forEach((el) => {
+        const key = el.getAttribute("data-i18n");
+
+        const keyParts = key.split(".");
+        let value = data;
+
+        keyParts.forEach((part) => {
+          value = value ? value[part] : undefined;
+        });
+
+        if (value !== undefined) {
+          el.innerHTML = value;
+        }
+      });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => setLanguage("en"));
